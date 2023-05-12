bf5e2ec8cff8bd044d407f75fd2fc24c416891a9
unknown <baternguyen.2407@gmail.com>
2023-05-12 16:08:52 +0700

    Load

--- src/main/java/ptithcm/controller/StaffController.java
+++ src/main/java/ptithcm/controller/StaffController.java
@@ -1,6 +1,7 @@
 package ptithcm.controller;
 
 import java.sql.Date;
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -9,17 +10,20 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import javax.transaction.Transactional;
 
+import org.hibernate.Hibernate;
 import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import ptithcm.model.CTDotGiamGia;
+import ptithcm.model.DonDatHang;
 import ptithcm.model.DotGiamGia;
 import ptithcm.model.LoaiSanPham;
 import ptithcm.model.NhaCungCap;
@@ -253,5 +257,192 @@ public class StaffController {
 		Query query = session.createQuery(hql);
 		model.addAttribute("listNCC", query.list());
 		return "staff/danhsachnhacungcap";
+	}
+	
+	@RequestMapping(value = "danhsachnhanvien")
+	public String danhsachnhanvien(ModelMap model) {
+		org.hibernate.Session session = factory.getCurrentSession();
+		String hql =  "FROM NhanVien";
+		Query query = session.createQuery(hql);
+		List<NhanVien> list =	query.list(); 
+		model.addAttribute("DSNhanVien", list);
+		return "staff/danhsachnhanvien";
+	}
+	
+	
+	@RequestMapping("themnhanvien")
+	public String Showthemnhanvien()
+	{
+		return "staff/themnhanvien";
+	}
+	@RequestMapping(value = "/themnhanvien", method = RequestMethod.POST)
+	public String themnhanvien(HttpServletRequest request, Model model)
+	{
+		String maNV = request.getParameter("Id");
+		  String ho = request.getParameter("Ho"); 
+		  String ten = request.getParameter("Ten"); String chucVu = request.getParameter("CV");
+		  String email = request.getParameter("email");
+		  String SDT = request.getParameter("SDT"); 
+		  String Bdate= request.getParameter("birthdate");
+		  Date ngaySinh = Date.valueOf(Bdate); 
+		  String diaChi =request.getParameter("diaChi");
+		 
+		System.out.println("CHAY222");
+		Session session = factory.getCurrentSession();
+		
+		String hql = "FROM NhanVien WHERE maNV = :maNV";
+		Query query = session.createQuery(hql);
+		query.setParameter("maNV", maNV);
+
+		if (query.list().size() == 0) {
+			
+			 NhanVien nhanVien = new NhanVien(maNV, ho, ten, ngaySinh, SDT,email,"12345678",diaChi,1,chucVu);
+			 
+
+			session.save(nhanVien); 
+			model.addAttribute("ErrorId", "Thêm nhân viên mới thành công!");
+			return "staff/themnhanvien";
+		}
+		else {
+
+			model.addAttribute("ErrorId", "Vui lòng chọn mã nhân viên khác! Mã này đã được sử dụng!");
+			model.addAttribute("email", email); 
+			model.addAttribute("Ho", ho);
+			model.addAttribute("Ten", ten); 
+			model.addAttribute("birthdate", ngaySinh);
+			model.addAttribute("SDT", SDT); 
+			model.addAttribute("diaChi", diaChi);
+			model.addAttribute("CV", chucVu);
+			 
+			return "staff/themnhanvien";
+		}
+		
+		
+	}
+	@RequestMapping("suathongtinnhanvien")
+	public String Showsuathongtinnhanvien(Model model, HttpServletRequest request)
+	{
+		Session session = factory.getCurrentSession();
+		String hql = "FROM NhanVien WHERE maNV = :maNV";
+	    Query query = session.createQuery(hql);
+	    query.setParameter("maNV", request.getParameter("idnv"));
+	    List<NhanVien> nhanVien = query.list();
+		model.addAttribute("nv", nhanVien.get(0));
+		return "staff/suathongtinnhanvien";
+	}
+	@RequestMapping(value = "suathongtinnhanvien",  method = RequestMethod.POST)
+	public String suathongtinnhanvien(HttpServletRequest request, Model model)
+	{
+		Session session = factory.getCurrentSession();
+		String hql = "FROM NhanVien WHERE maNV = :maNV";
+	    Query query = session.createQuery(hql);
+	    query.setParameter("maNV", request.getParameter("Id"));
+	    List<NhanVien> listNhanVien = query.list();
+	    NhanVien nhanVien = listNhanVien.get(0);
+	    nhanVien.setMaNV(request.getParameter("Id"));
+	    nhanVien.setHo(request.getParameter("Ho"));
+	    nhanVien.setTen(request.getParameter("Ten"));
+	    String Bdate= request.getParameter("birthdate");
+		 Date ngaySinh = Date.valueOf(Bdate); 
+		nhanVien.setNgaySinh(ngaySinh);
+		nhanVien.setDiaChi(request.getParameter("diaChi"));
+		nhanVien.setsDT(request.getParameter("SDT"));
+		nhanVien.setEmail(request.getParameter("email"));
+		nhanVien.setRole(request.getParameter("CV"));
+		model.addAttribute("nv", nhanVien);
+		model.addAttribute("ThongBao","Cập nhật thông tin thành công !");
+		session.update(nhanVien);
+		return "staff/suathongtinnhanvien";
+	}
+	
+	@RequestMapping(value = "dondathang")
+	public String dondathang(ModelMap model) {
+		org.hibernate.Session session = factory.getCurrentSession();
+		String hql =  "FROM DonDatHang";
+		Query query = session.createQuery(hql);
+		List<DonDatHang> list =	query.list(); 
+		
+		model.addAttribute("DSDDH", list);
+		
+	 	model.addAttribute("ThongBao","  ");
+		return "staff/dondathang";
+	}
+	
+	
+	@RequestMapping(value = "taodondathang")
+	public String showtaodondathang(ModelMap model) {
+		
+		org.hibernate.Session session = factory.getCurrentSession();
+		String hql =  "FROM NhaCungCap";
+		Query query = session.createQuery(hql);
+		List<NhanVien> DSNCC =	query.list(); 
+		hql = "FROM LoaiSanPham";
+		Query query2 = session.createQuery(hql);
+		List<LoaiSanPham> list2 =	query2.list(); 
+		model.addAttribute("DSNCC", DSNCC);
+		model.addAttribute("DSSP", list2);
+		return "staff/taodondathang";
+	}
+
+	@RequestMapping("chitietdondathang")
+	public String showchitietdondathang(Model model, HttpServletRequest request)
+	{
+		Session session = factory.getCurrentSession();
+		String hql = "FROM DonDatHang WHERE maDDH = :maDDH";
+	    Query query = session.createQuery(hql);
+	    query.setParameter("maDDH", request.getParameter("maDDH"));
+	    List<DonDatHang> donDatHang = query.list();
+        Hibernate.initialize(donDatHang.get(0).getCtDonDatHang());
+		model.addAttribute("DDH", donDatHang.get(0));
+		return "staff/chitietdondathang";
+	}
+	@RequestMapping("detetedondathang")
+		public String dondatHangDeleted(Model model, HttpServletRequest request)
+		{
+		
+			Session session = factory.getCurrentSession();
+			String hql = "FROM DonDatHang WHERE maDDH = :maDDH";
+		    Query query = session.createQuery(hql);
+		    query.setParameter("maDDH", request.getParameter("idDDH"));
+		    DonDatHang donDat = (DonDatHang) query.uniqueResult();
+		    if (donDat.getSoPhieuNhap() == null)
+		    {
+		    	String hql1 = "DELETE FROM DonDatHang WHERE maDDH = :maDDH";
+		    	Query query1 = session.createQuery(hql1);
+		    	query1.setParameter("maDDH", request.getParameter("idDDH"));
+		    	int rowsAffected = query1.executeUpdate();
+		    	model.addAttribute("ThongBao","Xóa đơn đặt hàng thành công !");
+		    	
+		    }
+		    else 
+		    {
+		    	model.addAttribute("ThongBao","Đơn hàng đã có phiếu nhập không thể xóa !");
+		  
+		    }
+		    
+		    String hql2 =  "FROM DonDatHang";
+			Query query2 = session.createQuery(hql2);
+			List<DonDatHang> list =	query2.list(); 
+			model.addAttribute("DSDDH", list);
+		    
+			return "staff/dondathang";
+		}
+	 ArrayList<LoaiSanPham> listSP = new ArrayList<LoaiSanPham>();
+	 ArrayList<Integer> listSL = new ArrayList<Integer>();
+	@RequestMapping("taotabledondathang")
+	public String taotabledondathang(Model model, HttpServletRequest request)
+	{
+		Session session = factory.getCurrentSession();
+		String hql = "FROM LoaiSanPham WHERE maLoai = :maLoai";
+	    Query query = session.createQuery(hql);
+	    query.setParameter("maLoai", request.getParameter("sanPham"));
+	    LoaiSanPham SP = (LoaiSanPham) query.uniqueResult();
+	    listSP.add(SP);
+	    Integer SL = Integer.parseInt(request.getParameter("soLuong"));
+	    listSL.add(SL);
+		model.addAttribute("listSP",listSP);
+		model.addAttribute("listSL",listSL);
+		model.addAttribute("doLon",listSL.size()-1);
+		return "staff/taodondathang";
 	}
 }
---
+++ src/main/webapp/WEB-INF/views/staff/suathongtinnhanvien.jsp
@@ -0,0 +1,130 @@
+<%@page import="ptithcm.model.NhanVien"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<%@taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Zay Shop eCommerce HTML CSS Template</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<link rel="apple-touch-icon"
+	href="<c:url value='/resource/assets/img/apple-icon.png'/>">
+<link rel="shortcut icon" type="image/x-icon"
+	href="<c:url value='/resource/assets/img/favicon.ico'/>">
+
+<link rel="stylesheet"
+	href="<c:url value='/resource/assets/css/bootstrap.min.css'/>">
+<link rel="stylesheet"
+	href="<c:url value='/resource/assets/css/templatemo.css'/>">
+<link rel="stylesheet"
+	href="<c:url value='/resource/assets/css/custom.css'/>">
+
+<!-- Load fonts style after rendering the layout styles -->
+<link rel="stylesheet"
+	href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;200;300;400;500;700;900&display=swap">
+<link rel="stylesheet"
+	href="<c:url value='/resource/assets/css/fontawesome.min.css'/>">
+<!--
+
+-->
+</head>
+<body>
+<%@include file="headerNV.jsp"%>
+ <!-- Modal -->
+    <div class="modal fade bg-white" id="templatemo_search" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-lg" role="document">
+            <div class="w-100 pt-1 mb-5 text-right">
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <form action="" method="get" class="modal-content modal-body border-0 p-0">
+                <div class="input-group mb-2">
+                    <input type="text" class="form-control" id="inputModalSearch" name="q" placeholder="Search ...">
+                    <button type="submit" class="input-group-text bg-success text-light">
+                        <i class="fa fa-fw fa-search text-white"></i>
+                    </button>
+                </div>
+            </form>
+        </div>
+    </div>
+
+
+    <!-- Start Content Page -->
+    <main>
+          <section class="bg-light">
+       <div class = "container pd-5">
+       <div class="card">
+       	<div class="card-body">
+            <div >
+                <h2>Cập Nhật Thông Tin Nhân Viên</h2>
+              </div>
+         <form class="employee-list" action="suathongtinnhanvien.htm" method="POST">
+         <h6 style="color: red; margin-left: 15px">${ThongBao }</h6>
+	<!-- Start Contact -->
+    <div class="container py-5" lang="vi">
+        <div class="row py-5">
+            
+                <div class="row">
+                    <div class="form-group col-md-6 mb-3">
+                        <label for="inputId">Mã Nhân Viên :</label>
+                        <input type="text" class="form-control mt-1" id="Id" name="Id" value="${nv.maNV}" readonly="readonly">
+                    </div>
+                    <div class="form-group col-md-6 mb-3">
+                        <label for="selectChucVu" >Chức Vụ :</label>
+                        <br>
+                        <select type ="text" id="CV" name="CV">
+						    <option value="NVBanHang" ${nv.role == "NVBanHang" ? "selected" : ""}>Nhân Viên Bán Hàng</option>
+						    <option value="NVBaoHanh" ${nv.role == "NVBaoHanh" ? "selected" : ""}>Nhân Viên Bảo Hành</option>
+						    <option value="NVGiaoHang" ${nv.role == "NVGiaoHang" ? "selected" : ""}>Nhân Viên Giao Hàng</option>
+						    <option value="QL" ${nv.role == "QL" ? "selected" : ""}>Quản Lí</option>
+						</select>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="form-group col-md-6 mb-3">
+                        <label for="inputHo">Họ :</label>
+                        <input type="text" class="form-control mt-1" id="Ho" name="Ho" value="${nv.ho}" placeholder="Vui Lòng Không Bỏ Trống" required>
+                    </div>
+                    <div class="form-group col-md-6 mb-3">
+                        <label for="inputTen">Tên :</label>
+                        <input type="text" class="form-control mt-1" id="Ten" name="Ten" value="${nv.ten}" placeholder="Vui Lòng Không Bỏ Trống" required>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="form-group col-md-6 mb-3">
+                        <label for="birthdate">Ngày sinh:</label><br>
+			            <input type="date" id="birthdate" name="birthdate" value="${nv.ngaySinh}" placeholder="dd/mm/yyyy" pattern="\d{2}/\d{2}/\d{4}" required>
+                    </div>
+                    <div class="form-group col-md-6 mb-3">
+                        <label for="inputSDT">Số Điện Thoại :</label>
+                        <input type="tel" class="form-control mt-1" id="SDT" name="SDT" value="${nv.sDT}" pattern="[0-9]{10}" placeholder="Vui Lòng Không Bỏ Trống" title="Số điện thoại phải bao gồm 10 chữ số" required>
+                    </div>
+                </div>
+
+                <div class="mb-3">
+                    <label for="inputEmail">Email :</label>
+                    <input type="email" class="form-control mt-1" id="email" name="email" value="${nv.email }" placeholder="Vui Lòng Không Bỏ Trống" required>
+                </div>
+                <div class="mb-3">
+                    <label for="inputDiaChi">Địa Chỉ :</label>
+                    <input type="text" class="form-control mt-1" id="diaChi" name="diaChi" value="${nv.diaChi }"  placeholder="Vui Lòng Không Bỏ Trống" required>
+                </div>
+                <div class="row">
+                    <div class="col text-end mt-2">
+                        <button type="submit" class="btn btn-success btn-lg px-3">Lưu  <i class="fa fa-check" aria-hidden="true"></i></button>
+                        <!-- <button type="submit" class="btn btn-success btn-lg px-3">Hủy</button> -->
+                    </div>
+                </div>
+        </div>
+    </div>
+    </form>
+    </div>
+    </div>
+    </div>
+    </section>
+    </main>
+    <!-- End Contact -->
+    <%@include file="footerQL.jsp" %>
+</body>
+</html>
